3dd32dce2e049186dd4e98cf2cc96250adfab158
Laura <laurakovacic@outlook.com>
2023-03-21 23:12:36 +0100

    testing Spring Test Context

--- src/test/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplicationTests.java
+++ src/test/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplicationTests.java
@@ -1,10 +1,31 @@
 package com.laurakovacic.spring6dependencyinjectiondemo;
 
+import com.laurakovacic.spring6dependencyinjectiondemo.controllers.ControllerDemo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DependencyInjectionDemoApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+    
+    @Autowired
+    ControllerDemo controller;
+
+    @Test
+    void getControllerFromCtxTest() {
+        ControllerDemo controller = applicationContext.getBean(ControllerDemo.class);
+
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void autowireOfControllerTest() {
+        System.out.println(controller.sayHello());
+    }
 
     @Test
     void contextLoads() {
