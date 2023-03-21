292452c9e9bf9c364a503aca4b07886ef333896c
Laura <laurakovacic@outlook.com>
2023-03-21 22:49:52 +0100

    initial commit

---
+++ src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.laurakovacic.spring6dependencyinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DependencyInjectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6DependencyInjectionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurakovacic.spring6dependencyinjectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DependencyInjectionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
