f54175aac4ebc1b69d4d345c8926fb7ba11cebc8
Laura <laurakovacic@outlook.com>
2023-03-21 23:08:01 +0100

    testing Spring Context

--- src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplication.java
+++ src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/Spring6DependencyInjectionDemoApplication.java
@@ -1,13 +1,21 @@
 package com.laurakovacic.spring6dependencyinjectiondemo;
 
+import com.laurakovacic.spring6dependencyinjectiondemo.controllers.ControllerDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DependencyInjectionDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DependencyInjectionDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Spring6DependencyInjectionDemoApplication.class, args);
+
+        ControllerDemo controller = ctx.getBean(ControllerDemo.class);
+
+        System.out.println("In Main Method");
+
+        System.out.println(controller.sayHello());
     }
 
 }
