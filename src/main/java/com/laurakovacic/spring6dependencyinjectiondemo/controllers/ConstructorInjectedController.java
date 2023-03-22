a9bf906bc66f59536153066059b7b6bc5e9126a6
Laura <laurakovacic@outlook.com>
2023-03-22 07:46:43 +0100

    dependency injection using Spring

--- src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.laurakovacic.spring6dependencyinjectiondemo.controllers;
 
 import com.laurakovacic.spring6dependencyinjectiondemo.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/laurakovacic/spring6dependencyinjectiondemo/controllers/PropertyInjectedController.java
@@ -1,9 +1,12 @@
 package com.laurakovacic.spring6dependencyinjectiondemo.controllers;
 
 import com.laurakovacic.spring6dependencyinjectiondemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
-
+    @Autowired
     GreetingService greetingService;
 
     public String sayHello() {
